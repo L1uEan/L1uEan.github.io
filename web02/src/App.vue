4e38c61ce7413339f6913759e3af389e38ae1884
liuyan0815 <1411343694@qq.com>
2020-07-06 16:24:49 +0800

    7.6VUE4.4.6版本项目

---
+++ web02/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+}
+body{
+  font-size: 0.14rem;
+  color: red;
+}
+</style>
